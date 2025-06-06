ebd54d2e7aa1107b826b8a077cfe1be2fb82f56d
Joy <joybrar2001@gmail.com>
2025-06-06 11:36:57 +0530

    Added User Cards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -4,11 +4,11 @@ export default function AdminPage() {
   return (
     <main className="flex flex-col md:flex-row gap-4 p-4">
       <div className="w-full lg:w-2/3">
-        <div className="flex gap-4 justify-between">
-          <UserCard type="student" />
-          <UserCard type="teacher" />
-          <UserCard type="parent" />
-          <UserCard type="staff" />
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="Student" />
+          <UserCard type="Teacher" />
+          <UserCard type="Parent" />
+          <UserCard type="Staff" />
         </div>
       </div>
 
--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -1,9 +1,32 @@
+import Image from "next/image";
+
 export default function UserCard({
   type
 }: {
-  type: string;
+  type: "Student" | "Teacher" | "Parent" | "Staff";
 }) {
   return (
-    <div className="rounded-2xl odd:bg-purple even:bg-yellow">User Card</div>
+    <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4 flex-1 min-w-[130px]">
+      <div className="flex justify-between items-center">
+        <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
+          2024/25
+        </span>
+
+        <Image
+          src={"/more.png"}
+          alt="more"
+          width={20}
+          height={20}
+        />
+      </div>
+
+      <h1 className="text-2xl font-semibold my-4">
+        1,234
+      </h1>
+
+      <h2 className="text-sm font-medium text-gray-500">
+        {type}s
+      </h2>
+    </div>
   );
 }
