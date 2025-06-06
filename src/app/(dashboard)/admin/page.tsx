bfc0a484885dfc146b5132ff53e479245abd271d
Joy <joybrar2001@gmail.com>
2025-06-06 12:04:12 +0530

    Added Radial Chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,18 +1,46 @@
+// import Announcements from "@/components/Announcements";
+// import AttendanceChart from "@/components/AttendanceChart";
+// import EventCalendar from "@/components/EventCalendar";
+// import FinanceChart from "@/components/FinanceChart";
+import CountChart from "@/components/charts/count-chart";
 import UserCard from "@/components/user-card";
 
-export default function AdminPage() {
+const AdminPage = () => {
   return (
-    <main className="flex flex-col md:flex-row gap-4 p-4">
-      <div className="w-full lg:w-2/3">
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="Student" />
           <UserCard type="Teacher" />
           <UserCard type="Parent" />
           <UserCard type="Staff" />
         </div>
+
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+
+          <div className="w-full lg:w-2/3 h-[450px]">
+            {/* <AttendanceChart /> */}
+            AttendanceChart
+          </div>
+        </div>
+
+        <div className="w-full h-[500px]">
+          {/* <FinanceChart /> */}
+          FinanceChart
+        </div>
       </div>
 
-      <div className="w-full lg:w-1/3">r</div>
-    </main>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        {/* <EventCalendar /> */}
+        EventCalendar
+        {/* <Announcements/> */}
+        Announcements
+      </div>
+    </div>
   );
-}
+};
+
+export default AdminPage;
